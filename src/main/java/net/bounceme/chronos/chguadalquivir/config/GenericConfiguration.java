b1a9dfcbf5d797ea44276e0c432415a2d9c0697f
Federico Martín Lara <federico@horizontefinal>
2023-01-27 21:59:41 +0100

    Revert last commit

--- src/main/java/net/bounceme/chronos/chguadalquivir/config/GenericConfiguration.java
+++ src/main/java/net/bounceme/chronos/chguadalquivir/config/GenericConfiguration.java
@@ -2,6 +2,7 @@ package net.bounceme.chronos.chguadalquivir.config;
 
 import java.text.SimpleDateFormat;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import net.bounceme.chronos.chguadalquivir.support.Constants;
 
 @Configuration
+@EnableBatchProcessing
 public class GenericConfiguration {
 
 	@Bean
