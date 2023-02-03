dd7ca770a61ae2376491838d6e36d96bd66ed7b5
Federico Martín Lara <federico@horizontefinal>
2023-02-03 11:11:47 +0100

    Fixed bug

--- src/main/java/net/bounceme/chronos/chguadalquivir/reader/mapping/StatExecutionsFieldMapper.java
+++ src/main/java/net/bounceme/chronos/chguadalquivir/reader/mapping/StatExecutionsFieldMapper.java
@@ -1,9 +1,11 @@
 package net.bounceme.chronos.chguadalquivir.reader.mapping;
 
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.stereotype.Component;
 
 import net.bounceme.chronos.chguadalquivir.model.Execution;
 
+@Component
 public class StatExecutionsFieldMapper extends BeanWrapperFieldSetMapper<Execution> {
 
 	public StatExecutionsFieldMapper() {
