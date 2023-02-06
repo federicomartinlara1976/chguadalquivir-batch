6f482150d01b1be3e73632f0c8d61b02c9b322d3
Federico Martín Lara <federico@horizontefinal>
2023-02-06 17:30:32 +0100

    Get last job instance by name

--- src/main/java/net/bounceme/chronos/chguadalquivir/controller/JobController.java
+++ src/main/java/net/bounceme/chronos/chguadalquivir/controller/JobController.java
@@ -4,15 +4,21 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -39,6 +45,12 @@ public class JobController {
 	
 	@Autowired
 	private JobLauncher jobLauncher;
+	
+	@Autowired
+	private JobRepository jobRepository;
+	
+	@Autowired
+	private JobExplorer jobExplorer;
 
 	/**
 	 * @return
@@ -83,6 +95,30 @@ public class JobController {
 		}
 	}
 	
+	@PostMapping("/last")
+	public ResponseEntity<Map<String, Object>> lastJob(@Valid @RequestBody Task task, BindingResult result) {
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			if (result.hasErrors()) {
+				List<String> errors = result.getFieldErrors().stream()
+						.map(error -> String.format("El campo '%s' %s", error.getField(), error.getDefaultMessage()))
+						.collect(Collectors.toList());
+	
+				response.put("errors", errors);
+				return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+			}
+			
+			log.info("Buscar: {}", task.getName());
+			JobInstance jobInstance = getLastJobInstance(task.getName());
+			response.put("jobInstance", jobInstance);
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (Exception e) {
+			response.put("error", e.getMessage());
+			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 	/**
 	 * @param name
 	 * @throws Exception
@@ -102,5 +138,16 @@ public class JobController {
 		} catch (NoSuchBeanDefinitionException e) {
 			throw new Exception("Tarea no encontrada");
 		}
+	}
+	
+	/**
+	 * @param name
+	 * @return
+	 * @throws Exception
+	 */
+	private JobInstance getLastJobInstance(String name) {
+		List<JobInstance> jobInstances = jobExplorer.getJobInstances(name, 0, 10);
+		
+		return (CollectionUtils.isNotEmpty(jobInstances)) ? jobInstances.get(0) : null;
 	}
 }
