9ff8f7b04dadba2ea879c3fae37dc078146d9b57
MAMALAFE <f.martin@ibermatica.com>
2023-04-19 20:31:06 +0200

    Connection pool configuration

--- src/main/java/net/bounceme/chronos/chguadalquivir/config/PostgresDataSourceConfiguration.java
+++ src/main/java/net/bounceme/chronos/chguadalquivir/config/PostgresDataSourceConfiguration.java
@@ -3,6 +3,8 @@ package net.bounceme.chronos.chguadalquivir.config;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -17,6 +19,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import net.bounceme.chronos.chguadalquivir.model.ExecutionStats;
 
+import java.beans.PropertyVetoException;
+
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
@@ -26,15 +30,17 @@ import net.bounceme.chronos.chguadalquivir.model.ExecutionStats;
 )
 public class PostgresDataSourceConfiguration {
 
-	@Bean
-	@ConfigurationProperties("spring.datasource.postgres")
-	public DataSourceProperties postgresDataSourceProperties() {
-		return new DataSourceProperties();
-	}
-
 	@Bean(name = "postgresDataSource")
-	public DataSource postgresDataSource() {
-		return postgresDataSourceProperties().initializeDataSourceBuilder().build();
+	public DataSource postgresDataSource(@Autowired C3P0Properties c3P0Properties) throws PropertyVetoException {
+		ComboPooledDataSource pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setDriverClass(c3P0Properties.getDriverClass());
+		pooledDataSource.setUser(c3P0Properties.getUser());
+		pooledDataSource.setPassword(c3P0Properties.getPassword());
+		pooledDataSource.setJdbcUrl(c3P0Properties.getJdbcUrl());
+		pooledDataSource.setMaxPoolSize(c3P0Properties.getMaxPoolSize());
+		pooledDataSource.setMaxIdleTime(c3P0Properties.getMaxIdleTime());
+
+		return pooledDataSource;
 	}
 	
 	@Bean(name = "postgresEntityManagerFactory")
