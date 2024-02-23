2da7fa190f38d009627543224af48885521dc799
Federico Martín Lara <federico@horizontefinal>
2024-02-23 09:42:13 +0100

    Se actualiza controller

--- src/main/java/net/bounceme/chronos/chguadalquivir/controller/JobController.java
+++ src/main/java/net/bounceme/chronos/chguadalquivir/controller/JobController.java
@@ -3,7 +3,6 @@ package net.bounceme.chronos.chguadalquivir.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -18,79 +17,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import net.bounceme.chronos.chguadalquivir.model.ExecutionResult;
 import net.bounceme.chronos.chguadalquivir.model.Task;
 import net.bounceme.chronos.chguadalquivir.services.JobService;
 
-@CrossOrigin(origins = {"*"})
+@CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/api/jobs")
 @Slf4j
 public class JobController {
-	
+
 	@Autowired
 	private JobService jobService;
-	
+
 	@PostMapping("/execute")
+	@SneakyThrows
 	public ResponseEntity<Map<String, Object>> executeTask(@Valid @RequestBody Task task, BindingResult result) {
 		Map<String, Object> response = new HashMap<>();
-		
-		try {
-			if (result.hasErrors()) {
-				List<String> errors = result.getFieldErrors().stream()
-						.map(error -> String.format("El campo '%s' %s", error.getField(), error.getDefaultMessage()))
-						.collect(Collectors.toList());
-	
-				response.put("errors", errors);
-				return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-			}
-			
-			log.info("Ejecutar: {}", task.getName());
-			ExecutionResult resultado = jobService.run(task.getName());
-			response.put("resultado", resultado);
-			return new ResponseEntity<>(response, HttpStatus.OK);
-		} catch (Exception e) {
-			response.put("error", e.getMessage());
-			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+
+		log.info("Ejecutar: {}", task.getName());
+		ExecutionResult resultado = jobService.run(task.getName());
+		response.put("resultado", resultado);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/scheduling")
 	public ResponseEntity<Map<String, Object>> schedulingJob(@Valid @RequestBody Task task, BindingResult result) {
 		Map<String, Object> response = new HashMap<>();
-		
-		try {
-			if (result.hasErrors()) {
-				List<String> errors = result.getFieldErrors().stream()
-						.map(error -> String.format("El campo '%s' %s", error.getField(), error.getDefaultMessage()))
-						.collect(Collectors.toList());
-	
-				response.put("errors", errors);
-				return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-			}
-			
-			log.info("Cron de {}", task.getName());
-			String scheduling = jobService.getJobScheduling(task.getName());
-			response.put("scheduling", scheduling);
-			return new ResponseEntity<>(response, HttpStatus.OK);
-		} catch (Exception e) {
-			response.put("error", e.getMessage());
-			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+
+		String scheduling = jobService.getJobScheduling(task.getName());
+		log.debug("Cron de {}: {}", task.getName(), scheduling);
+		response.put("scheduling", scheduling);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("")
 	public ResponseEntity<Map<String, Object>> getJobs() {
 		Map<String, Object> response = new HashMap<>();
-		
-		try {
-			List<String> jobs = jobService.getAllJobs();
-			response.put("jobs", jobs);
-			return new ResponseEntity<>(response, HttpStatus.OK);
-		} catch (Exception e) {
-			response.put("error", e.getMessage());
-			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+
+		List<String> jobs = jobService.getAllJobs();
+		response.put("jobs", jobs);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
