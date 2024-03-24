a87464c2a515bc1c28ffa5273a21c3f76ca81edd
Federico Martín Lara <federico@horizontefinal>
2024-03-24 12:16:06 +0100

    Se quita el CORSya que está gestionado por la pasarela

--- src/main/java/net/bounceme/chronos/chguadalquivir/controller/IndexController.java
+++ src/main/java/net/bounceme/chronos/chguadalquivir/controller/IndexController.java
@@ -3,14 +3,12 @@ package net.bounceme.chronos.chguadalquivir.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.bounceme.chronos.chguadalquivir.model.Status;
 
-@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/api")
 public class IndexController {
