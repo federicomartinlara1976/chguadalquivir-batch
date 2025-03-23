4fe02e2fd908e6e759b3ffec93258e8f4fb27421
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2025-03-23 22:02:44 +0100

    Flujo y componentes

---
+++ src/main/java/net/bounceme/chronos/chguadalquivir/processor/RegistroPluviometriaValidator.java
@@ -0,0 +1,37 @@
+package net.bounceme.chronos.chguadalquivir.processor;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import net.bounceme.chronos.chguadalquivir.model.RegistroDiarioPluviometria;
+import net.bounceme.chronos.chguadalquivir.validation.ValidatorService;
+
+/**
+ * @author federico
+ *
+ */
+@Component
+@Slf4j
+public class RegistroPluviometriaValidator implements Validator<RegistroDiarioPluviometria> {
+	
+	@Autowired
+	private ValidatorService<RegistroDiarioPluviometria> validatorService;
+
+	@Override
+	public void validate(RegistroDiarioPluviometria value) {
+		try {
+			validatorService.validate(value);
+		} catch (ConstraintViolationException e) {
+			for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+				log.error(violation.getMessage());
+			}
+			throw new ValidationException(String.format("El registro [%s] no se va a procesar", value.getCodigo()));
+		}
+	}
+}
